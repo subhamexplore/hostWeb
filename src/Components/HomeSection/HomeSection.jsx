54185ed67e88a9838ca3eb0257eb2bfa30f6a7da
Dinesh Rout <rddinesh779@gmail.com>
2023-06-18 01:30:16 +0530

    styles: responsiveness added ✨

--- src/Components/HomeSection/HomeSection.jsx
+++ src/Components/HomeSection/HomeSection.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import "./HomeSection.scss"
+import React from 'react';
+import './HomeSection.scss';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const HomeSection = ({
-    title,
-    titleColor,
-	description,
-    buttonText,
-    buttonBackground
+  title,
+  titleColor,
+  description,
+  buttonText,
+  buttonBackground,
 }) => {
   return (
     <div className='home-section'>
-        <div className='title' style={{ color: titleColor }}>{title}</div>
-        <div className='description'>{description}</div>
-        <div className='button' style={{backgroundColor:buttonBackground}} >
-            <button className="home-section-btn" style={{backgroundColor:buttonBackground}}>{buttonText}  
-            </button>
-            <ArrowForwardIcon className='arrow-icon'/>
+      {title && (
+        <div className='title' style={{ color: titleColor }}>
+          {title}
         </div>
+      )}
+      {description && <div className='description'>{description}</div>}
+      <div className='button' style={{ backgroundColor: buttonBackground }}>
+        <button
+          className='home-section-btn'
+          style={{ backgroundColor: buttonBackground }}
+        >
+          {buttonText}
+        </button>
+        <ArrowForwardIcon className='arrow-icon' />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeSection
+export default HomeSection;

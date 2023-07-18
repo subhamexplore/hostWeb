7c7209091ce0872014762895f99353aa03a47d0e
priyanka1971 <psenapati441@gmail.com>
2023-07-18 17:08:26 +0530

    package-lock.json 1

---
+++ src/Pages/Alumni/Alumni.js
@@ -0,0 +1,193 @@
+import React from 'react'
+import "./Alumni.css"
+import { FaAlignJustify } from "react-icons/fa";
+import { FaRegEnvelope } from "react-icons/fa";
+
+
+const Alumni = () => {
+    return (
+
+        <div className="container">
+
+            <div className='icon'>
+                <FaAlignJustify />
+            </div>
+            <div className="upper">
+                <div className="upperleft">
+                    <h1>lets meet our alumnis</h1>
+
+                </div>
+                <div className="upperright">
+
+                    <p>React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.</p>
+                </div>
+            </div>
+            <div className='lower'>
+             
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+
+
+                        <p>Co-ordinator</p>
+
+                        <FaRegEnvelope id="icon" />
+
+                    </div>
+                </div>
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+               
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+               
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+                
+                
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+                
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+                
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+               
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+
+
+                        <p>Co-ordinator</p>
+
+                        <FaRegEnvelope id="icon" />
+
+                    </div>
+                </div>
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+               
+                <div className="cards">
+                    <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5ODIyNTAxfHxlbnwwfHx8fHw%3D&w=1000&q=80" alt="" srcset="" />
+                    <div className="desc">
+                        <h3>Title</h3>
+                        <p>Co-ordinator</p>
+                        <FaRegEnvelope id="icon" />
+                    </div>
+                </div>
+               
+             
+                
+
+
+
+
+            </div>
+
+
+
+
+
+        </div>
+
+
+
+
+
+    );
+};
+
+export default Alumni;

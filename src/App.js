395526d1cdcd93c655c9f397c52dc66743467599
Pallavinehaaa23 <pallavisahoo263@gmail.com>
2023-07-18 17:30:34 +0530

    about us 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route,Routes} from "react-router-dom";
 import Main from "./Pages/Main/Main";
 import Blogs from './Pages/Blogs/Blogs';
-import AboutUs from './Pages/AboutUs/AboutUs';
+
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/blogs" element={<Blogs/>} />
-        <Route path="/about" element={<AboutUs/>} />
+      
       </Routes>
     </div>
   );
